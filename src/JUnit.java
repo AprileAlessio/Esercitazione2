e092c8588c439c89f1993d57143207a4b162f40b
aless <aless@DESKTOP-PE0ND7B.lan>
2020-05-12 15:05:32 +0200

    Versione 1

---
+++ src/JUnit.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnit {
+
+	@Test
+	void testCalcolaArea() {
+		fail("Not yet implemented");
+	}
+
+}
